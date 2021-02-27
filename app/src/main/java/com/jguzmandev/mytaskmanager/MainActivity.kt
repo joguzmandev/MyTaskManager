590a14fb1c47ac077326c64996d1c6dfac5a6cbf
JorgeGuzmanDev <jorgeguzman2210@gmail.com>
2021-02-27 16:01:36 -0400

    Init project

---
+++ app/src/main/java/com/jguzmandev/mytaskmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jguzmandev.mytaskmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
